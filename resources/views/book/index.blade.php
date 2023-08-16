b4a1364d221d9c0c59beea35f148cd953d6ff298
NguyenQuan791 <quannguyen1879@gmail.com>
2023-08-16 11:01:41 +0700

    - Login MVC

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -3,14 +3,16 @@
 
 <head>
     <title>Book</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <style>
-        *{
+        * {
             outline: none;
             padding: 0;
             margin: 0;
         }
 
-        body{
+        body {
             padding: 30px;
         }
 
@@ -102,11 +104,29 @@
         }
     </style>
 </head>
-
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+                <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit">Logout</button>
+                </form>
+            </div>
+        </div>
+    </nav>
     <center>
-    <h1>Sách nói Monkey - Thông tin</h1>
-    <h1>Sách nói Monkey - Thông tin</h1>
+        <h1>Sách nói Monkey - Thông tin</h1>
     </center>
     <a href="{{url('/book/create')}}"><button style="width:200px;height:50px;margin-bottom:10px; background-color: green; color:white;">Thêm sách</button></a>
     <table style="width:100%">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,9 +13,7 @@ use App\Http\Controllers\BookController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('',function(){
-    return view('welcome');
-});
+Route::get('',[BookController::class,'index']);
 
 Route::resource('book',BookController::class);
 
@@ -34,5 +33,16 @@ Route::get('book/edit/{id}',[BookController::class,'edit']);
 
 //     Route::delete('/delete/{id}', [BookController::class,'deleteItem']);
 // });
-
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/book', [BookController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('',[BookController::class,'index']);
+});
 
